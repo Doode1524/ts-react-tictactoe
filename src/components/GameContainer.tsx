1253edf0c5dc5e0cb9d56aefd94dcd1d31aac837
joey <doode1524@gmail.com>
2021-06-08 20:21:59 -0400

    import JS Gamecontainer logic

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -1,12 +1,89 @@
-import React, {useState, useEffect} from 'react'
-import Gameboard from './Gameboard'
-
+import React, { useState, useEffect } from "react";
+import Gameboard from "./Gameboard";
 const GameContainer = () => {
-    return (
-        <div>
-            <Gameboard />
-        </div>
-    )
-}
+  const [turn, setTurn] = useState("X");
+  const [winner, setWinner] = useState(null);
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
 
-export default GameContainer
+  const winningCombo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // eslint-disable-next-line no-use-before-define
+  const handleClick = (e) => {
+    if (e.target.innerHTML === "" && turn === "X") {
+      e.target.innerHTML = turn;
+      const newArry = board;
+      newArry[e.target.id - 1] = turn;
+      setBoard(newArry);
+      setTurn("O");
+    } else if (e.target.innerHTML === "" && turn === "O") {
+      e.target.innerHTML = turn;
+      const newArry = board;
+      newArry[e.target.id - 1] = turn;
+      setBoard(newArry);
+      setTurn("X");
+    }
+  };
+  useEffect(() => checkWinningCombo(), [handleClick]);
+
+  const resetButton = (e) => {
+    const boxes = document.querySelectorAll(".gameboard__box");
+
+    boxes.forEach((element) => {
+      element.innerHTML = "";
+      element.style.pointerEvents = "auto";
+    });
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setWinner(null);
+    document.getElementById("winner").style.display = "none";
+    document.getElementById("turn").style.display = "block";
+    setTurn("X");
+  };
+
+  const checkWinningCombo = () => {
+    // eslint-disable-next-line array-callback-return
+    return winningCombo.find((combo) => {
+      if (
+        board[combo[0]] !== "" &&
+        board[combo[1]] !== "" &&
+        board[combo[2]] !== "" &&
+        board[combo[0]] === board[combo[1]] &&
+        board[combo[1]] === board[combo[2]]
+      ) {
+        const boxes = document.querySelectorAll(".gameboard__box");
+        boxes.forEach((element) => {
+          element.style.pointerEvents = "none";
+        });
+        setWinner(board[combo[0]]);
+        displayWinner();
+        return turn;
+      }
+    });
+  };
+
+  const displayWinner = () => {
+    if (winner !== null) {
+      document.getElementById("winner").style.display = "block";
+      document.getElementById("turn").style.display = "none";
+    }
+  };
+  return (
+    <div>
+      <Gameboard
+        turn={turn}
+        handleClick={handleClick}
+        resetButton={resetButton}
+        winner={winner}
+      />
+    </div>
+  );
+};
+
+export default GameContainer;
